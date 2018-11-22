781ef24cb63c8d2c77b1e7b4c305de3d2719ca26
Johan Revilla <johan.revilla.92@gmail.com>
2018-11-22 00:23:38 -0300

    initializing the proyect

---
+++ src/tp_5_catedra_juarez.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : tp_5_catedra_juarez.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
